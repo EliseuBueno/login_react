5f07fd03b1b6f7ae1daf7770a28cdd0ec5a9bf54
Eliseu Bueno <sturarobuenoeliseu@gmail.com>
2022-11-12 16:58:40 -0300

    Feat: Criação de estrutura de rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import RouteApp from "./routes";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <>
+            <RouteApp />
+        </>
+    );
 };
 
 export default App;
